d21e6d61d45c43563cf253b0d6a9dacd27422271
Shameerminhas1 <122668054+Shameerminhas1@users.noreply.github.com>
2024-02-19 17:37:01 +0500

    19 feb

--- pixelpen/lib/SplashScreen.dart
+++ pixelpen/lib/SplashScreen.dart
@@ -1,1 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:pixelpen/welcomescreen.dart';
 
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+  bool _animationCompleted = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeIn,
+    );
+
+    _animationController.forward().then((value) {
+      setState(() {
+        _animationCompleted = true;
+      });
+      if (_animationController.isCompleted) {
+        navigateToWelcomeSCreen();
+      }
+    });
+  }
+
+  void navigateToWelcomeSCreen() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => WelcomeScreen()),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: FadeTransition(
+          opacity: _animation,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/img.png',
+                width: 250,
+                height: 250,
+              ),
+              // const SizedBox(height: 15),
+              // AnimatedOpacity(
+              //   opacity: _animationCompleted ? 0.0 : 1.0,
+              //   duration: const Duration(milliseconds: 500),
+              // ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
